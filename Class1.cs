226ef6fe6fc5918999eb0830e1fe29a4b42da03c
Sintawee69 <sintawee.kit@sbac.ac.th>
2023-06-07 14:53:39 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int mylnt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+            Console.WriteLine(Convert.ToString(mylnt));
+            Console.WriteLine(Convert.ToDouble(mylnt));
+            Console.WriteLine(Convert.Tolnt32(myDouble));
+            Console.WriteLine(Convert.ToString(myBool));
+        }
+    }
+
+}
+//Console.WriteLine("Hello, World!");
---
+++ Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int iterator = 0; iterator < 3; iterator++)
+            {
+                Console.WriteLine("Outer Loop:"+iterator);
+                {
+                    for (int _iterartor = 0; _iterartor<2; ++_iterartor)
+                    {
+                        Console.WriteLine("lnner Loop:"+iterator);
+                    }
+                }
+            }
+
+            String[] brandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+
+            foreach (string television in brandTV)
+            {
+                Console.WriteLine("Brand"+television);
+            }
+        }
+    }
+}
